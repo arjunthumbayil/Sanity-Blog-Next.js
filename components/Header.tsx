d68ae5158fe7f91b76a9b283388672be32357995
arjunthumbayil <arjunthumbayil@gmail.com>
2022-07-05 22:41:33 +0530

    Navbar completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,18 +3,21 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
-      <div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
         <Link href='/'>
             <img className="w-44 cursor-pointer object-contain" src="https://links.papareact.com/yvf" alt=""></img>
         </Link>
-        <div>
+        <div className="hidden md:inline-flex items-center space-x-5">
             <h3>About</h3>
             <h3>Contact</h3>
-            <h3>Follow</h3>
+            <h3 className="text-white bg-green-600 px-4 py-1 rounded-full" >Follow</h3>
         </div>
       </div>
-      <div></div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="border rounded-full px-4 py-1 border-green-600">Get Started</h3>
+      </div>
     </header>
   );
 }
